ebbf80feb6de8f1dc69dc998a7253f5e55ded48f
George Flores <georgeflores886@gmail.com>
2020-11-09 15:47:06 -0500

    created utils.js for songs

---
+++ src/utils.js
@@ -0,0 +1,99 @@
+import { v4 as uuidv4} from 'uuid'
+
+function lofiHop() {
+    return[
+        {
+            name: 'Again',
+            cover:'https://chillhop.com/wp-content/uploads/2020/07/2a1f30f0720752596707aaba08fe100e7658de70-1024x1024.jpg',
+            artist: 'H.1',
+            audio: 'https://mp3.chillhop.com/serve.php/?mp3=8498',
+            color: ['#AF4F47', '#FDEDCB'],
+            id: uuidv4(),
+            active: true,
+        },
+        {
+            name: '',
+            cover:'',
+            artist:'',
+            audio:'',
+            color: [],
+            id: uuidv4(),
+            active:false
+        },
+        {
+            name: '',
+            cover:'',
+            artist:'',
+            audio:'',
+            color: [],
+            id: uuidv4(),
+            active:false
+        },
+
+        {
+            name: '',
+            cover:'',
+            artist:'',
+            audio:'',
+            color: [],
+            id: uuidv4(),
+            active:false
+        },
+        {
+            name: '',
+            cover:'',
+            artist:'',
+            audio:'',
+            color: [],
+            id: uuidv4(),
+            active:false
+        },
+        {
+            name: '',
+            cover:'',
+            artist:'',
+            audio:'',
+            color: [],
+            id: uuidv4(),
+            active:false
+        },
+        {
+            name: '',
+            cover:'',
+            artist:'',
+            audio:'',
+            color: [],
+            id: uuidv4(),
+            active:false
+        },
+        {
+            name: '',
+            cover:'',
+            artist:'',
+            audio:'',
+            color: [],
+            id: uuidv4(),
+            active:false
+        },
+        {
+            name: '',
+            cover:'',
+            artist:'',
+            audio:'',
+            color: [],
+            id: uuidv4(),
+            active:false
+        },
+        {
+            name: '',
+            cover:'',
+            artist:'',
+            audio:'',
+            color: [],
+            id: uuidv4(),
+            active:false
+        }
+
+    ]
+}
+export default lofiHop
