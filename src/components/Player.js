91d3bb3831a85e4c3b2f705c8490465f16cc7bae
George Flores <georgeflores886@gmail.com>
2020-11-10 10:32:29 -0500

    updated state of input range and play/pause button

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,6 +1,6 @@
 import React, {useRef, useState} from 'react'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faPlay, faAngleLeft, faAngleRight} from '@fortawesome/free-solid-svg-icons'
+import {faPlay, faAngleLeft, faAngleRight, faPause} from '@fortawesome/free-solid-svg-icons'
 
 const Player = ({currentSong, isPlaying, setIsPlaying}) => {
     const audioRef = useRef(null)
@@ -29,21 +29,24 @@ const Player = ({currentSong, isPlaying, setIsPlaying}) => {
 
     //State for song time
     const [songInfo, setSongInfo] = useState({
-        currentTime: null,
-        duration: null
+        currentTime: 0,
+        duration: 0
     })
     
-
+    const dragHandler = e => {
+        audioRef.current.currentTime = e.target.value
+        setSongInfo({...songInfo, currentTime:e.target.value})
+    }
     return (
         <div className='player'>
             <div className="time-control">
                 <p>{getTime(songInfo.currentTime)}</p>
-                <input type="range"/>
+                <input min={0} max={songInfo.duration} value={songInfo.currentTime} onChange={dragHandler} type="range"/>
                 <p>{getTime(songInfo.duration)}</p>
             </div>
             <div className="play-control">
                 <FontAwesomeIcon className='skip-back' size='2x'icon={faAngleLeft}/>
-                <FontAwesomeIcon onClick={playSongHandler} className='play' size='2x' icon={faPlay}/>
+                <FontAwesomeIcon onClick={playSongHandler} className='play' size='2x' icon={isPlaying ? faPause : faPlay}/>
                 <FontAwesomeIcon className='skip-forward' size='2x' icon={faAngleRight}/>
             </div>
             <audio onTimeUpdate={timeUpdateHandler} onLoadedMetadata={timeUpdateHandler} ref= {audioRef} src={currentSong.audio}></audio>
