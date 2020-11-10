a9ec8487393a4b0153cf5c58ce605b41361abf28
George Flores <georgeflores886@gmail.com>
2020-11-10 09:57:40 -0500

    created song and play states and passeed to components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Song from './components/Song'
 import Player from './components/Player'
 import data from './utils'
 
 import './styles/app.scss'
+
 function App() {
+  //State
+const [songs, setSongs] = useState(data())
+const [currentSong, setCurrentSong] = useState(songs[0])
+
   return (
     <div className="App">
       <h1>Music Player</h1>
-      <Song/>
-      <Player />
+      <Song currentSong={currentSong} />
+      <Player currentSong= {currentSong} />
     </div>
   );
 }
