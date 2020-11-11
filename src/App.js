14761687767751204369336f1cfcf6eae0053605
George Flores <georgeflores886@gmail.com>
2020-11-11 11:23:33 -0500

    added song end handler to skip forward

--- src/App.js
+++ src/App.js
@@ -33,6 +33,13 @@ const timeUpdateHandler = (e) => {
   console.log(animation)
   setSongInfo({...songInfo, currentTime: current, duration, animationPercentage: animation})
 }
+//skip forward to next song
+const songEndHandler = async () => {
+  let currentIndex = songs.findIndex((song) => song.id === currentSong.id);
+      //if song reaches end of index, go back go 0 index (first song in library)
+   await setCurrentSong(songs[(currentIndex + 1) % songs.length] );
+  if(isPlaying) audioRef.current.play();    
+}
 
   return (
     <div className="App">
@@ -40,7 +47,7 @@ const timeUpdateHandler = (e) => {
       <Song currentSong={currentSong} />
       <Player setSongs={setSongs} songs={songs} audioRef ={audioRef} setIsPlaying={setIsPlaying} isPlaying={isPlaying} currentSong= {currentSong} setSongInfo={setSongInfo} songInfo= {songInfo} setCurrentSong={setCurrentSong} />
       <Library audioRef={audioRef} songs={songs} setCurrentSong={setCurrentSong} isPlaying={isPlaying} setSongs={setSongs} libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus}/>
-      <audio onTimeUpdate={timeUpdateHandler} onLoadedMetadata={timeUpdateHandler} ref= {audioRef} src={currentSong.audio}></audio>
+      <audio onTimeUpdate={timeUpdateHandler} onLoadedMetadata={timeUpdateHandler} ref= {audioRef} src={currentSong.audio} onEnded={songEndHandler}></audio>
     </div>
   );
 }
