9c64215222dec93844c980d1e5b105d6879bce41
George Flores <georgeflores886@gmail.com>
2020-11-11 12:46:54 -0500

    updated background

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import { faMusic} from '@fortawesome/free-solid-svg-icons'
+import { faMusic, faHeadphones} from '@fortawesome/free-solid-svg-icons'
 
 const Nav = ({libraryStatus, setLibraryStatus}) => {
     return (
         <nav>
-            <h1>Lofi Music</h1>
+            <h1>Lofi Station <FontAwesomeIcon icon={faHeadphones} /></h1>
             <button onClick={() => setLibraryStatus(!libraryStatus)}>
                 Library
                 <FontAwesomeIcon icon ={faMusic} />
