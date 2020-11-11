59867b453e41ad85ebf859ef4df0640880aebfa6
George Flores <georgeflores886@gmail.com>
2020-11-11 11:11:47 -0500

    updated input range style

--- src/App.js
+++ src/App.js
@@ -18,7 +18,8 @@ const [isPlaying, setIsPlaying] = useState(false)
 //State for song time
 const [songInfo, setSongInfo] = useState({
   currentTime: 0,
-  duration: 0
+  duration: 0,
+  animationPercentage: 0,
 })
 const [libraryStatus, setLibraryStatus] = useState(false)
 
@@ -26,7 +27,11 @@ const [libraryStatus, setLibraryStatus] = useState(false)
 const timeUpdateHandler = (e) => {
   const current = e.target.currentTime
   const duration = e.target.duration
-  setSongInfo({...songInfo, currentTime: current, duration})
+  const roundedCurrent = Math.round(current)
+  const roundedDuration = Math.round(duration)
+  const animation = Math.round((roundedCurrent / roundedDuration)* 100)
+  console.log(animation)
+  setSongInfo({...songInfo, currentTime: current, duration, animationPercentage: animation})
 }
 
   return (
--- src/components/Player.js
+++ src/components/Player.js
@@ -76,12 +76,19 @@ const Player = ({audioRef, currentSong, isPlaying, setIsPlaying, setSongInfo, so
         playAudio(isPlaying, audioRef)
         //console.log(currentIndex + 1)
     }
+    //Add styles
+    const trackAnim ={
+        transform: `translateX(${songInfo.animationPercentage}%)`
+    }
 
     return (
         <div className='player'>
             <div className="time-control">
                 <p>{getTime(songInfo.currentTime)}</p>
+                <div style={{background: `linear-gradient(to right, ${currentSong.color[0]}, ${currentSong.color[1]})`}} className='track'>
                 <input min={0} max={songInfo.duration || 0} value={songInfo.currentTime} onChange={dragHandler} type="range"/>
+                <div style={trackAnim} className="animate-track"></div>
+                </div>
                 <p>{songInfo.duration ? getTime(songInfo.duration) : '0:00'}</p>
             </div>
             <div className="play-control">
