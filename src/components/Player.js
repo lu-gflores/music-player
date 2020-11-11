fffc65199c3f11f3486682d2d7bf1a9cd8384621
George Flores <georgeflores886@gmail.com>
2020-11-11 11:42:02 -0500

    removed useeffect

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,35 +1,34 @@
-import React, {useEffect} from 'react'
+import React from 'react'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faPlay, faAngleLeft, faAngleRight, faPause} from '@fortawesome/free-solid-svg-icons'
 
 const Player = ({audioRef, currentSong, isPlaying, setIsPlaying, setSongInfo, songInfo, songs, setCurrentSong, setSongs}) => {
-   
-    //useEffect
-    useEffect(() => {
-        const newSongs = songs.map((song) => {
-            if(song.id === currentSong.id){
-                return {
-                    ...song,
-                    active: true,
-                }
-            } else {
-                return {
-                    ...song,
-                    active: false,
-                }
+   const activeLibraryHandler = (nextPrev) => {
+    const newSongs = songs.map((song) => {
+        if(song.id === nextPrev.id){
+            return {
+                ...song,
+                active: true,
             }
-        })
-
-        if(isPlaying) {
-            const playPromise = audioRef.current.play()
-            if(playPromise !== undefined) {
-                playPromise.then((audio) => {
-                    audioRef.current.play()
-                })
+        } else {
+            return {
+                ...song,
+                active: false,
             }
         }
-        setSongs(newSongs)
-    }, [currentSong])
+    })
+
+    if(isPlaying) {
+        const playPromise = audioRef.current.play()
+        if(playPromise !== undefined) {
+            playPromise.then((audio) => {
+                audioRef.current.play()
+            })
+        }
+    }
+    setSongs(newSongs)
+   }
+    
     //Event Handlers
     const playSongHandler = () => {
         if(isPlaying) {
@@ -60,18 +59,21 @@ const Player = ({audioRef, currentSong, isPlaying, setIsPlaying, setSongInfo, so
 
         if(direction ==='skip-forward') {
             //if song reaches end of index, go back go 0 index (first song in library)
-           await setCurrentSong(songs[(currentIndex + 1) % songs.length] )
+           await setCurrentSong(songs[(currentIndex + 1) % songs.length])
+           activeLibraryHandler(songs[(currentIndex + 1) % songs.length])
         }
 
         if(direction === 'skip-back') {
             //if song is the first in array, go to last song in the array instead
             if((currentIndex - 1) % songs.length === -1) {
                 await setCurrentSong(songs[songs.length - 1 ]);
+                activeLibraryHandler(songs[songs.length - 1 ])
                 if(isPlaying) audioRef.current.play();
                 return; //with this return, it keeps the next line from running and crashing the app
             }
             //go back one song
           await setCurrentSong(songs[(currentIndex - 1) % songs.length] )
+          activeLibraryHandler(songs[(currentIndex - 1) % songs.length] )
         }
         if(isPlaying) audioRef.current.play();
         //console.log(currentIndex + 1)
